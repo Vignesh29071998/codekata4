1f5a538204d58c7e1f8d19fc7ebea4709d6fafff
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-02-13 23:06:19 +0530

    Update 15_decreasing_number.py

--- 15_decreasing_number.py
+++ 15_decreasing_number.py
@@ -1,5 +1,17 @@
+from collections import Counter
 a=int(input())
 b=list(map(int,input().split()))
-c=sorted(b,reverse=True)
-for i in c:
+for i in range(0,len(b)-1):
+  for j in range(i+1,len(b)):
+    m,n=bin(b[i]),bin(b[j])
+    m1=Counter(m)
+    m1=m1['1']
+    n1=Counter(n)
+    n1=n1['1']
+    if m1<=n1:
+      if b[i]<b[j]:
+        b[i],b[j]=b[j],b[i]
+for i in b:
   print(i)
+    
+  
