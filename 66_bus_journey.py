0f6a73a6d4dac1f0434d3dff2723ecffa1f879bb
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-13 23:17:37 +0530

    Update 66_bus_journey.py

--- 66_bus_journey.py
+++ 66_bus_journey.py
@@ -1,12 +1,28 @@
 a,b,f,k = map(int,input().split())
-flag=0
-for i in range(0,k):
-  if abs(0-f)*2 <=b and abs(f-a)*2<=b:
-    flag=1
-  else:
-    flag=0
-    break
-if flag==1:
-  print(k)
-else:
+c,gas = 0,b
+l = abs(0-f)
+gas = gas-l
+for i in range(1,k+1):
+  if i%2!=0:
+    if i==k:
+      if abs(f-a) > gas:
+        c = c+1
+    elif abs(f-a)*2 > gas:
+      gas = b-abs(f-a)*2
+      c = c+1
+    else:
+      gas = gas-abs(f-a)*2
+  elif i%2==0:
+    if i==k:
+      if abs(0-f) > gas:
+        c = c+1
+    elif abs(0-f)*2 > gas:
+      gas = b-abs(0-f)*2
+      c = c+1
+    else:
+      gas = gas-abs(0-f)*2
+if gas < 0:
   print('-1')
+else:
+  print(c)
+  
