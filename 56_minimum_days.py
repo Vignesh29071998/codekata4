c89ff26178e8a0474391339653f032698b87bc5f
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-14 11:17:05 +0530

    Update 56_minimum_days.py

--- 56_minimum_days.py
+++ 56_minimum_days.py
@@ -2,8 +2,11 @@ n, k = map(int, input().split())
 days, c = 1, 0
 a = list(map(int, input().split()))
 for i in range(0, n):
-  b = 86400-a[i]+c
-  if b != k:
-    c = 86400-a[i]
+  b = 86400-a[i]
+  c = c+b
+  if c!= k:
     days = days+1
+  else:
+    break
 print(days)
+
