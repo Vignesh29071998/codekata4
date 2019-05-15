64ebe5b07f94d46bbc6e8a94c79e44ebed6094e9
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-15 15:43:18 +0530

    Create 35_k_dominant_character.py

---
+++ 35_k_dominant_character.py
@@ -0,0 +1,28 @@
+a = input()
+b,c,flag,count = '',[],0,0
+for i in range(0,len(a)-1):
+  count +=1
+  j = 0
+  while j < len(a)-i:
+    if j!=len(a)-i-1:
+      b = a[j:j+i+1]
+      c.append(b)
+      j = j+1
+    else:
+      c.append(a[j:j+i+1])
+      j = j+1
+  l = c[0]
+  for i in range(0,len(l)):
+    for j in range(1,len(c)):
+      if c[0][i] in c[j]:
+        flag = 1
+      else:
+        flag = 0
+        break
+  c = []
+  if flag == 1:
+    print(count)
+    break
+  else:
+    continue
+    
