6880ca4a4cc51339c061f66d3ebc78556b7f85f8
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-15 12:16:41 +0530

    Update 47_k_rounding_of_n.py

--- 47_k_rounding_of_n.py
+++ 47_k_rounding_of_n.py
@@ -1,13 +1,19 @@
 n,k = map(int,input().split())
 k1 = [10,20,40,80,160,320,640,1280]
-if k == 0:
+c = 0
+if k == 0 or k==1:
   print(n)
 else:
   a = n*k1[k-1]
   b = n*10**k
   if a%n == 0:
     c = str(a)
-    if c[-k:]=='0'*k:
+    for i in c:
+      if i=='0':
+        c = c+1
+      else:
+        c =0
+    if c>=len('0'*k):
       print(a)
     else:
       print(n*10**k)
