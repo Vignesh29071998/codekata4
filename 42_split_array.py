0ed3d0da5be8be96b236b7700a26cecf8007aa79
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-15 11:23:20 +0530

    Create 42_split_array.py

---
+++ 42_split_array.py
@@ -0,0 +1,20 @@
+n,k = map(int,input().split())
+l = list(map(int,input().split()))
+a,b,ma = [],[],[]
+def split(a,m):
+  mi = 0
+  for i in range(0,m):
+    mi = min(a[i])
+    b.append(mi)
+  ma.append(max(b))
+  return ma
+for i in range(0,n-1):
+  for j in range(0,k):
+    if j%2==0:
+      a.append(l[:i+1])
+    else:
+      a.append(l[i+1:])
+  b = []
+  c = split(a,k)
+  a = []
+print(max(c))
