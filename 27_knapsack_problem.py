c103635e03b2bf78826e4de5fedcb9b62ebd5541
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-28 23:40:55 +0530

    Update 27_knapsack_problem.py

--- 27_knapsack_problem.py
+++ 27_knapsack_problem.py
@@ -1,12 +1,13 @@
 n,w = map(int,input().split())
 value = list(map(int,input().split()))
 weight = list(map(int,input().split()))
-c,m = 0,0
+c,m,p = 0,0,1
 for i in range(0,n-1):
   for j in range(i+1,n):
-    for k in range(i+1,n-j+1):
+    for k in range(i+1,n-j+p):
       su = value[i]+sum(value[k:k+j])
       m = weight[i]+sum(weight[k:k+j])
       if su <= w and m>c:
         c = m
+  p += 1
 print(c)
